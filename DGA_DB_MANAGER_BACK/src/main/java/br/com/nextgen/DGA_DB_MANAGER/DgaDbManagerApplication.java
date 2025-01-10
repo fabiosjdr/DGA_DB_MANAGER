55c3624acf90d9e94c1adfccbe8c665ad4297235
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 <fabioruffs@gmail.com>
2025-01-10 07:28:40 -0500

    tratamento multi tanant

--- DGA_DB_MANAGER_BACK/src/main/java/br/com/nextgen/DGA_DB_MANAGER/DgaDbManagerApplication.java
+++ DGA_DB_MANAGER_BACK/src/main/java/br/com/nextgen/DGA_DB_MANAGER/DgaDbManagerApplication.java
@@ -3,10 +3,22 @@ package br.com.nextgen.DGA_DB_MANAGER;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DgaDbManagerApplication {
 
 	public static void main(String[] args) {
+
+
+		Dotenv dotenv = Dotenv.configure().load();
+	
+        // Configurar variáveis de ambiente
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(DgaDbManagerApplication.class, args);
 	}
 
